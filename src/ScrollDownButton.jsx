f93103a141d2be445393f776c6214ece3c3a1722
rakakroma <102795114+rakakroma@users.noreply.github.com>
2023-04-17 16:47:57 +0800

    more concise

--- src/ScrollDownButton.jsx
+++ src/ScrollDownButton.jsx
@@ -1,23 +1,6 @@
-import { useEffect, useState } from "react";
 import { ChevronDoubleDownIcon } from "@heroicons/react/20/solid";
 
 export const ScrollDownButton = () => {
-  // const [isHidden, setIsHidden] = useState(false);
-
-  // const toggleHidden = () => {
-  //   const isAtBottom =
-  //     document.documentElement.scrollHeight -
-  //       document.documentElement.clientHeight -
-  //       window.pageYOffset <=
-  //     60;
-  //   isAtBottom ? setIsHidden(true) : setIsHidden(false);
-  // };
-
-  // useEffect(() => {
-  //   window.addEventListener("scroll", toggleHidden);
-  //   return () => window.removeEventListener("scroll", toggleHidden);
-  // }, []);
-
   return (
     <div
       className={`scroll-button fixed left-0 right-0 bottom-0 mx-auto z-50 text-gray-300 w-[120px] mb-3 text-center`}
--- src/useResize.jsx
+++ src/useResize.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useLayoutEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const debounce = (fn, t = 250) => {
   let timeout = null;
